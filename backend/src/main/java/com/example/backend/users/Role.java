3671a985554fe927e8d69ef2d2183f59ade858d3
Matty <nagamallimathi@gmail.com>
2025-06-24 22:42:30 +0530

    Changes

--- backend/src/main/java/com/example/backend/users/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.users;
-
-public enum Role {
-  USER,
-  ADMIN
-}
--- frontend/models/http/HttpErrorResponse.ts
+++
@@ -1,6 +0,0 @@
-export interface HttpErrorResponse {
-  message: string;
-  status: number;
-  errors?: Map<string, string>;
-  generalErrors?: string[];
-}
